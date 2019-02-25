6971ab9251750e0eb5feaaca9ebd8ce405038e99
kvivas <kvivas@zava.com.ve>
2019-02-25 19:52:07 -0400

    agregando link a backend

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         new webpack.DefinePlugin({
             // global app config object
             config: JSON.stringify({
-                apiUrl: 'http://localhost:8000'
+                apiUrl: 'https://ancient-springs-30332.herokuapp.com/'
             })
         })
     ],
